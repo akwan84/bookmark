e156c7e02b18e746c59d468bc2ba195d0d022947
Andrew Kwan <akwan84@uwo.ca>
2024-12-25 14:29:10 -0500

    hashed passwords

--- src/main/java/com/andrew/bookmark/service/mapper/UserMapper.java
+++ src/main/java/com/andrew/bookmark/service/mapper/UserMapper.java
@@ -2,14 +2,16 @@ package com.andrew.bookmark.service.mapper;
 
 import com.andrew.bookmark.dto.UserDto;
 import com.andrew.bookmark.entity.User;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper {
     public User toUser(UserDto dto) {
+        String hashedPw = BCrypt.hashpw(dto.password(), BCrypt.gensalt());
         return new User(
                 dto.username(),
-                dto.password()
+                hashedPw
         );
     }
 }
