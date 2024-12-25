00880b7edf543246f829179d9ed966116f060177
Andrew Kwan <akwan84@uwo.ca>
2024-12-25 15:21:05 -0500

    created login endpoint

--- src/main/java/com/andrew/bookmark/controller/UserController.java
+++ src/main/java/com/andrew/bookmark/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.andrew.bookmark.controller;
 
 import com.andrew.bookmark.dto.UserDto;
+import com.andrew.bookmark.entity.User;
 import com.andrew.bookmark.service.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -17,8 +25,23 @@ public class UserController {
         this.service = service;
     }
 
-    @PostMapping("/user")
+    @PostMapping("/register")
     public void registerUser(@Valid @RequestBody UserDto dto) {
         this.service.registerUser(dto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@Valid @RequestBody UserDto dto) {
+        return this.service.loginUser(dto);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handleUserNotFoundException(
+            ResponseStatusException e
+    ) {
+        Map<String, String> message = new HashMap<>();
+        message.put("status", e.getStatusCode() + "");
+        message.put("message", e.getReason());
+        return new ResponseEntity<>(message, e.getStatusCode());
     }
 }
--- src/main/java/com/andrew/bookmark/service/service/UserService.java
+++ src/main/java/com/andrew/bookmark/service/service/UserService.java
@@ -4,8 +4,14 @@ import com.andrew.bookmark.dto.UserDto;
 import com.andrew.bookmark.entity.User;
 import com.andrew.bookmark.repository.UserRepository;
 import com.andrew.bookmark.service.mapper.UserMapper;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,5 +28,20 @@ public class UserService {
     public void registerUser(UserDto dto){
         User user = this.mapper.toUser(dto);
         this.repository.save(user);
+    }
+
+    public ResponseEntity<String> loginUser(UserDto dto) {
+        Optional<User> foundUser = this.repository.findByUsername(dto.username());
+        if(!foundUser.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+
+        boolean match = BCrypt.checkpw(dto.password(), foundUser.get().getPassword());
+
+        if(!match) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Password is incorrect");
+        }
+
+        return ResponseEntity.ok("Login Success");
     }
 }
